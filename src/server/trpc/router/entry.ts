4f487d755be3e038b176f6ea2bbf0040ba2804f0
jessejp <jessepernila@gmail.com>
2023-04-06 22:36:52 +0300

    initialize journal API

--- src/server/trpc/router/entry.ts
+++ src/server/trpc/router/entry.ts
@@ -2,6 +2,7 @@ import { router, protectedProcedure } from "../trpc";
 import z from "zod";
 
 const fieldInputValidation = z.object({
+  fieldId: z.string(),
   valueInteger: z.optional(z.number().nullable()),
   valueFloat: z.optional(z.number().nullable()),
   valueString: z.optional(z.string().max(510).nullable()),
@@ -35,6 +36,38 @@ export const entryRouter = router({
       return ctx.prisma.entry.create({
         data: {
           subjectId: input.subjectId,
+        },
+      });
+    }),
+});
+
+export const fieldRouter = router({
+  createField: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        fieldInputs: z.array(fieldInputValidation),
+      })
+    )
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.field.create({
+        data: {
+          name: input.name,
+        },
+      });
+    }),
+  createFieldInput: protectedProcedure
+    .input(fieldInputValidation)
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.fieldInput.create({
+        data: {
+          fieldId: input.fieldId,
+          valueInteger: input.valueInteger,
+          valueFloat: input.valueFloat,
+          valueString: input.valueString,
+          valueBoolean: input.valueBoolean,
+          unit: input.unit,
+          inputType: input.inputType,
         },
       });
     }),
