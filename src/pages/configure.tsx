19e50ef5f5bc5aa63ffd169fedb53992a902f679
jessejp <jessepernila@gmail.com>
2023-04-13 18:54:19 +0300

    Form tailwind style fixes

--- src/pages/configure.tsx
+++ src/pages/configure.tsx
@@ -7,6 +7,8 @@ import Heading from "../ui/Heading";
 import ButtonContainer from "../ui/ButtonContainer";
 import { useZodForm, subjectValidationSchema } from "../utils/useZodForm";
 
+const inputTypes = ["TEXTAREA", "WHOLE NUMBER", "FLOAT", "BOOLEAN", "RANGE"] as const;
+
 const Configure: NextPage = () => {
   const router = useRouter();
   const { data } = trpc.subject.getSubjects.useQuery();
@@ -27,26 +29,36 @@ const Configure: NextPage = () => {
   return (
     <Layout page="configure">
       <Heading>Profile</Heading>
-      <form>
-        <div className="flex flex-row flex-wrap justify-between my-2">
-          <label>Select Subject</label>
-          <select className="overflow-clip w-2/4 h-8"
-          {...form.register("subjectSelection")}>
+      <form className="flex flex-col w-10/12 justify-center content-center">
+        <div className="mt-2 mb-4 flex flex-row flex-wrap justify-around">
+          <label className="h-8 w-1/2 overflow-clip text-center text-lg font-bold text-zinc-200">
+            Select Subject
+          </label>
+          <select
+            className="h-8 w-1/2 overflow-clip"
+            {...form.register("subjectSelection")}
+          >
             <option value="Add New Subject">Add New Subject</option>
             {data?.map((subject) => (
-              <option key={subject.id} value={subject.id}>{subject.name}</option>
+              <option key={subject.id} value={subject.id}>
+                {subject.name}
+              </option>
             ))}
           </select>
         </div>
-        <div className="flex flex-row flex-wrap justify-between">
-          <label>Subject Name</label>
+        <div className="mt-2 mb-4 flex flex-row flex-wrap justify-around">
+          <label className="h-8 w-1/2 overflow-clip text-center text-lg font-bold text-zinc-200">
+            Subject Name
+          </label>
           <input
             type="text"
-            className="border-2 w-2/4"
+            className="w-2/4 border-2"
             {...form.register("subjectName")}
           />
           {form.formState.errors.subjectName && (
-            <p>{form.formState.errors.subjectName.message}</p>
+            <p className="text-red-500">
+              {form.formState.errors.subjectName.message}
+            </p>
           )}
         </div>
       </form>
@@ -54,14 +66,16 @@ const Configure: NextPage = () => {
         <Button
           intent="accept"
           action={form.handleSubmit(async (values) => {
-            if(values.subjectSelection === "Add New Subject") {
-            await addSubject.mutateAsync(values);
-          } else {
-            console.log('update subject')
-          }
+            if (values.subjectSelection === "Add New Subject") {
+              await addSubject.mutateAsync(values);
+            } else {
+              console.log("update subject");
+            }
           })}
         >
-          {form.formState.dirtyFields.subjectSelection ? "Update" : "Add New Subject"}
+          {form.formState.dirtyFields.subjectSelection
+            ? "Update"
+            : "Add New Subject"}
         </Button>
       </ButtonContainer>
     </Layout>
