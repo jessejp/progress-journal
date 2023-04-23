1804fa862efb4ac01ec68720cf549b7fe9f234f3
jessejp <jessepernila@gmail.com>
2023-04-23 10:45:29 +0300

    tailwind scroll install and implementation

--- src/ui/MainContent.tsx
+++ src/ui/MainContent.tsx
@@ -2,8 +2,8 @@ import React, { type PropsWithChildren } from "react";
 
 const MainContent: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <main className="my-3 flex h-full w-full justify-center overflow-scroll rounded bg-slate-800">
-      <div className="flex w-11/12 flex-col items-center md:w-4/5">
+    <main className="my-3 flex max-h-[32rem] w-full justify-center rounded bg-slate-800">
+      <div className="flex w-11/12 flex-col items-center overflow-y-scroll scrollbar-thin scrollbar-thumb-cyan-900 md:w-4/5">
         {children}
       </div>
     </main>
