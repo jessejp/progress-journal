4993f70fe7bcb86f713e499f41641bb6b9bdf744
jessejp <jessepernila@gmail.com>
2023-07-10 16:47:53 +0300

    feat: New Landing Page for Unauthed state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,25 +8,23 @@ import Heading from "../ui/Heading";
 import { trpc } from "../utils/trpc";
 import ButtonContainer from "../ui/ButtonContainer";
 import MainContent from "../ui/MainContent";
+import FrontPageLayout from "../ui/landing-page/FrontPageLayout";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
 
+  if (!sessionData) return <LandingPage sessionData={sessionData} />;
+
   return (
     <Layout page="home">
-      <Heading sessionData={sessionData}>
-        {sessionData ? "Progress Journal" : "Unauthorized"}
-      </Heading>
+      <Heading sessionData={sessionData}>Progress Journal</Heading>
 
       <MainContent>{sessionData && <Subjects />}</MainContent>
 
       <ButtonContainer>
-        {!sessionData && <AuthShowcase sessionData={sessionData} />}
-        {sessionData && (
-          <Button intent="open" link="/configure">
-            Configure
-          </Button>
-        )}
+        <Button intent="open" link="/configure">
+          Configure
+        </Button>
       </ButtonContainer>
     </Layout>
   );
@@ -38,7 +36,12 @@ const Subjects: React.FC = () => {
   const subjectsQuery = trpc.subject.getSubjects.useQuery();
   const { data } = subjectsQuery;
 
-  if(!data) return <p className="text-red-600">Create your first subject in order to start journaling!</p>
+  if (!data)
+    return (
+      <p className="text-red-600">
+        Create your first subject in order to start journaling!
+      </p>
+    );
 
   return (
     <>
@@ -58,15 +61,112 @@ const Subjects: React.FC = () => {
   );
 };
 
-const AuthShowcase: React.FC<{ sessionData: Session | null }> = (props) => {
+type SessionData = {
+  sessionData: Session | null;
+  text?: string;
+  icon?: string;
+};
+
+const AuthShowcase: React.FC<SessionData> = (props) => {
   const { sessionData } = props;
 
   return (
     <Button
       intent={sessionData ? "cancel" : "open"}
       action={sessionData ? () => signOut() : () => signIn()}
+      style="small rounded-full"
+      icon={props.icon}
     >
-      {sessionData ? "Sign out" : "Sign in"}
+      {!!props.text && props.text}
+      {!props.text && (sessionData ? "Sign out" : "Sign in")}
     </Button>
+  );
+};
+
+const LandingPage: React.FC<SessionData> = (props) => {
+  const { sessionData } = props;
+
+  return (
+    <FrontPageLayout>
+      <div className="relative flex flex-col items-center justify-between bg-hero bg-cover bg-center bg-no-repeat pb-4 text-center after:absolute after:z-0 after:min-h-mobileScreen after:w-full after:bg-gradient-to-t after:from-slate-800 after:from-50% after:via-slate-transparent after:via-70% after:to-slate-800 after:to-95%">
+        <div className="relative z-10 h-mobileScreenHalf w-full">
+          <h1 className="mt-4 font-bebasneue text-4xl text-lime-400">
+            PROGRESS JOURNAL
+          </h1>
+        </div>
+        <div className="bottom-0 z-10 flex w-11/12 flex-col justify-center gap-4 pb-6">
+          <h2 className="text-3xl font-bold text-zinc-100">
+            Progress Journal helps you to find your footing
+          </h2>
+          <AuthShowcase
+            sessionData={sessionData}
+            icon="discord.svg"
+            text="Sign in with Discord"
+          />
+          <p className="text-sm text-zinc-50">
+            Not a member yet?{" "}
+            <a
+              onClick={sessionData ? () => signOut() : () => signIn()}
+              className="cursor-pointer underline hover:text-lime-400"
+            >
+              Sign up here
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div className="relative z-10 flex flex-col items-center justify-end text-center">
+        <div className="h-80 w-full bg-mobile-user bg-cover bg-center"></div>
+        <div className="relative -top-14 flex w-11/12 flex-col justify-center gap-4 bg-slate-800 px-4 pb-6 pt-4 text-zinc-100">
+          <h2 className="text-3xl font-bold">
+            Progress Journal helps you to find your footing
+          </h2>
+          <p>
+            You can use Progress Journal to track your progress in any area!
+            It&apos;s a great way to keep yourself accountable and to see how
+            far you&apos;ve come.
+          </p>
+          <AuthShowcase
+            sessionData={sessionData}
+            icon="discord.svg"
+            text="Sign in with Discord"
+          />
+        </div>
+      </div>
+
+      <div className="relative flex flex-col items-center justify-end text-center">
+        <div className="flex w-full flex-col items-center justify-evenly gap-6 bg-exercising-user bg-cover bg-center pb-20 pt-6 after:absolute after:top-0 after:-z-0 after:h-4/5 after:w-full after:bg-lime-800 after:opacity-70 ">
+          <div className="z-10 flex w-11/12 flex-col items-center justify-center gap-1 py-2 text-center text-zinc-100">
+            <p>&quot;There are no shortcuts to any place worth going.&quot;</p>
+            <p className="text-sm">— Beverly Sills</p>
+          </div>
+
+          <div className="z-10 flex w-11/12 flex-col items-center justify-center gap-1 py-2 text-center text-zinc-100">
+            <p>
+              &quot;Training is about skill development, not about winning or
+              losing. You don’t need to win every battle, you only need to win
+              the battles that count.&quot;
+            </p>
+            <p className="text-sm">— John Danaher</p>
+          </div>
+        </div>
+
+        <div className="relative -top-14 flex w-11/12 flex-col justify-center gap-4 bg-slate-800 px-4 pb-6 pt-4 text-zinc-100">
+          <h3 className="text-3xl font-bold">
+            Progress Journal helps you to find your footing
+          </h3>
+        </div>
+      </div>
+      <div className="relative flex flex-col items-center justify-end gap-6 pb-20 text-center">
+        <h4 className="font-bebasneue text-5xl text-lime-400">
+          PROGRESS JOURNAL
+        </h4>
+        <AuthShowcase
+          sessionData={sessionData}
+          icon="discord.svg"
+          text="Sign in with Discord"
+        />
+      </div>
+    </FrontPageLayout>
   );
 };
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,12 +1,16 @@
 import type { MouseEventHandler, PropsWithChildren } from "react";
 import clsx from "clsx";
 import Link from "next/link";
+import Image from "next/image";
+
+type ButtonStyle = "default" | "small" | "xsmall" | "rounded-full";
 
 interface Props {
-  intent: "open" | "accept" | "cancel" | "selection"| "disabled" | "undo";
+  intent: "open" | "accept" | "cancel" | "selection" | "disabled" | "undo";
   action?: MouseEventHandler | undefined;
   link?: string;
-  style?: "default" | "small" | "xsmall";
+  style?: ButtonStyle | `${ButtonStyle} ${ButtonStyle}`;
+  icon?: string;
 }
 
 const Button: React.FC<PropsWithChildren<Props>> = ({
@@ -15,12 +19,15 @@ const Button: React.FC<PropsWithChildren<Props>> = ({
   action,
   link,
   style = "default",
+  icon,
 }) => {
   const variants = [
-    { "px-6 py-4 text-2xl": style === "default" },
-    { "px-4 py-2 text-xl": style === "small" },
-    { "px-2 py-1 text-sm": style === "xsmall" },
-    { "bg-blue-500 hover:bg-blue-700": intent === "open" },
+    { "px-8 py-3 text-xl": style.includes("default") === true },
+    { "px-6 py-2 text-base": style.includes("small") === true },
+    { "px-2 py-1 text-sm": style.includes("xsmall") === true  },
+    { "rounded-full": style.includes("rounded-full") === true },
+    { "rounded-lg": style.includes("rounded-full") === false },
+    { "bg-lime-400 hover:bg-lime-500": intent === "open" },
     { "bg-green-500 hover:bg-green-700": intent === "accept" },
     { "bg-red-500 hover:bg-red-700": intent === "cancel" },
     { "bg-indigo-500 hover:bg-indigo-700": intent === "undo" },
@@ -32,8 +39,20 @@ const Button: React.FC<PropsWithChildren<Props>> = ({
     return (
       <button
         onClick={action}
-        className={clsx("flex self-center rounded font-bold text-white", ...variants)}
+        className={clsx(
+          "flex self-center items-center font-bold text-zinc-800",
+          ...variants
+        )}
       >
+        {icon && (
+          <Image
+            src={`/images/${icon}`}
+            width={1}
+            height={1}
+            alt={`${icon.split(".")[0]} icon`}
+            className="mr-2 h-6 w-6 opacity-75"
+          />
+        )}
         {children}
       </button>
     );
@@ -41,7 +60,10 @@ const Button: React.FC<PropsWithChildren<Props>> = ({
     return (
       <Link
         href={link}
-        className={clsx("flex self-center rounded font-bold text-white", ...variants)}
+        className={clsx(
+          "flex self-center font-bold text-zinc-800",
+          ...variants
+        )}
       >
         {children}
       </Link>
